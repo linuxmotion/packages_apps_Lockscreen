e56d9822a0616e64e56b27c8383816e32111d9f9
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-05-30 21:39:30 -0700

    Remove import to allow compilation. This needs to be there to allw it to work correctly. The import sees the in eclipse fine, but when compiling it cannot find the class

--- src/com/android/security/lockscreen/Lockscreen.java
+++ src/com/android/security/lockscreen/Lockscreen.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
-import com.android.lockscreen.LockScreen;
+//import com.android.lockscreen.LockScreen;
 
 
 public class Lockscreen {
