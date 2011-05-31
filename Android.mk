db84b9a05a7de8ca40f0838049df8f4d34f1ab7c
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-05-30 22:25:39 -0700

    Set proper implemetation for compilation for core app

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android.policy
+#LOCAL_STATIC_JAVA_LIBRARIES := android.policy
 LOCAL_JAVA_LIBRARIES := android.policy
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
