beeaa31dec3eb8da090b610813f616f4194f0c26
Linuxmotion <linuxmotion@thelinuxmotion.org>
2011-05-31 22:39:27 -0700

    The class Lockscreen.java extends the LockScreenView. There is a minimal amount of the keyguard and related interfaces to allow usage as a lockscreen. LockScreenTester.java is the class that will test the new lockscreen to assure that the user can get past the screen. If the test passses than the user should be allowed to select it as a lockscreen replacement.

---
+++ src/com/android/security/lockscreen/LockScreenTester.java
@@ -0,0 +1,39 @@
+package com.android.security.lockscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class LockScreenTester extends Activity {
+	
+	
+	View mLockscreenView;
+	
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+	
+	
+	
+	
+	
+
+}
---
+++ src/com/android/security/lockscreen/Lockscreen.java
@@ -0,0 +1,48 @@
+package com.android.security.lockscreen;
+
+
+
+
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.android.internal.lockscreen.LockScreenView;
+import com.android.internal.policy.impl.KeyguardScreenCallback;
+import com.android.internal.policy.impl.KeyguardUpdateMonitor;
+import com.android.internal.telephony.IccCard.State;
+import com.android.internal.widget.LockPatternUtils;
+
+
+public class Lockscreen extends LockScreenView {
+
+	/**
+	 * 
+	 * 
+	 * @param context
+	 * @param configuration
+	 * @param lockPatternUtils
+	 * @param updateMonitor
+	 * @param callback
+	 */
+	Lockscreen(Context context, 
+			          Configuration configuration,
+						LockPatternUtils lockPatternUtils,
+							KeyguardUpdateMonitor updateMonitor, 
+								KeyguardScreenCallback callback) {
+		
+		
+		super(context, configuration, lockPatternUtils, updateMonitor, callback);
+		
+		
+		
+		
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
